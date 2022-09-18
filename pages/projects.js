04d6b514cd3f3cd9fa70e5939c819d05ab771790
horse <horsescary@gmail.com>
2022-09-17 20:23:06 -0400

    created "projects.js" script to format projects page

---
+++ pages/projects.js
@@ -0,0 +1,30 @@
+/*
+Assumes this structure exists: 
+<ul>
+    <li>
+        <a href="example.com">example</a>
+        <div>and example</div>
+    </li>
+</ul>
+
+creates div with class "link" with "a" as child
+adds class "description" to div
+*/
+
+window.onload = function() {
+    listItems = document.getElementsByTagName("li")
+    console.log(listItems)
+
+    for (i = 0; i < listItems.length; i++) {
+        item = listItems[i].children
+
+        div = document.createElement("div")
+        div.setAttribute("class", "link")
+
+        item[0].parentNode.insertAdjacentElement("afterbegin", div)
+        div.insertAdjacentElement("afterbegin", item[0])
+        
+        
+        item[1].setAttribute("class", "description")
+    }
+}
